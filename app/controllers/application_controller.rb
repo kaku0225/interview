b21ee130964c9ac17bae10a285b9d15c79e81895
eason <acontentedmind@gmail.com>
2021-02-17 21:42:58 +0800

    有登入才能做事情的驗證

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+  def session_required
+    redirect_to sign_in_users_path, notice: '請先登入會員' unless user_signed_in?
+  end
 end

2bd944a52650492e1c20fb9d659a91455348a741
eason <acontentedmind@gmail.com>
2021-02-17 21:39:56 +0800

    可編輯會員資料

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,6 +12,17 @@ class RegistrationsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path, notice: '資料更新成功'
+    else
+      render :edit
+    end
+  end
+
 
   private
 

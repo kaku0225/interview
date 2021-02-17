be4f58d8335339d99d0be3f92af3ff9ae428d316
eason <acontentedmind@gmail.com>
2021-02-17 20:56:27 +0800

    會員路徑

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   root 'homes#index'
+
+  resource :users, controller: 'registrations', only: [:create, :edit, :update] do
+    get '/sign_up', action: 'new'
+  end
+
+  resource :users, controller: 'sessions', only: [] do
+    get '/sign_in', action: 'new'
+    post '/sign_in', action: 'create'
+    delete '/sign_out', action: 'destroy'
+  end
 end

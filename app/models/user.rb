38db8c095c648a908d604abe24a33cb526cdc90e
eason <acontentedmind@gmail.com>
2021-02-17 21:21:28 +0800

    登入成功

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
   validates :password, presence: true, confirmation: true, length: { minimum: 4 }
   validates :nickname, presence: true
 
+  def self.login(user)
+    pw = Digest::SHA1.hexdigest("a#{user[:password]}Z")
+    User.find_by(email: user[:email], password: pw)
+  end
+
   private
     def encrypt_password
       self.password = Digest::SHA1.hexdigest("a#{self.password}Z")
